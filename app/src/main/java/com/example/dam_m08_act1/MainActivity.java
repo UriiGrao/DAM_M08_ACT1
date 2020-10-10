946fd17ffcca3baef8151b05d6052edfbf652d95
uriigrao <oriolgm375@gmail.com>
2020-10-10 16:20:43 +0200

    first spinner and button

--- app/src/main/java/com/example/dam_m08_act1/MainActivity.java
+++ app/src/main/java/com/example/dam_m08_act1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.dam_m08_act1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.StringPrepParseException;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> namesConvertor = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.deSpinner));
+        namesConvertor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //Añadir al spinner DE los items
+        Spinner spinnerDe = (Spinner) findViewById(R.id.spinnerDe);
+        spinnerDe.setAdapter(namesConvertor);
     }
 }
